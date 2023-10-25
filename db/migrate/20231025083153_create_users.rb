a4b6922b291ec80fd443e210bf9b168942101add
hafiz1379 <hafizrasa1379@gmail.com>
2023-10-25 14:04:32 +0430

    Create 'users' table with name, photo, bio, and posts.

--- db/migrate/20231025083153_create_users.rb
+++ db/migrate/20231025083153_create_users.rb
@@ -1,10 +1,12 @@
+# frozen_string_literal: true
+
 class CreateUsers < ActiveRecord::Migration[7.1]
   def change
     create_table :users do |t|
       t.string :name
       t.string :photo
       t.text :bio
-      t.integer :posts_counter
+      t.integer :post_counter
       t.timestamps
     end
   end
