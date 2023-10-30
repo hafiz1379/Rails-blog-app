8383de38f3a526c77e6eb46c03d70e08953bbd4d
hafiz1379 <hafizrasa1379@gmail.com>
2023-10-30 20:18:07 +0430

    Fix linter errors

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,9 @@
 class PostsController < ApplicationController
-    def index
-      # We want to show all posts
-    end
-  
-    def show
-      # We want to show a specific post
-    end
+  def index
+    # We want to show all posts
   end
+
+  def show
+    # We want to show a specific post
+  end
+end
