14d3ebfe0a89d760108737918010d87dbf806b4a
hafiz1379 <hafizrasa1379@gmail.com>
2023-10-30 20:15:34 +0430

    Updated

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,9 @@
 class PostsController < ApplicationController
-  def index
-    user = User.find(params[:user_id])
-
-    @posts = user.posts
+    def index
+      # We want to show all posts
+    end
+  
+    def show
+      # We want to show a specific post
+    end
   end
-
-  def show
-    @post = Post.find(params[:id])
-  end
-end
