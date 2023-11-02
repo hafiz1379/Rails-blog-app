8c6f3e7812eac584f53a41c62e4d7ceef873f9e0
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-02 19:22:12 +0430

    Update PostsController for displaying user-specific posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,17 @@
+# app/controllers/posts_controller.rb
+
 class PostsController < ApplicationController
   def index
-    # We want to show all posts
+    @user = User.find(params[:user_id])
+
+    @posts = @user.posts.page(params[:page]).per(3)
   end
 
   def show
-    # We want to show a specific post
+    @post = Post.find(params[:id])
+
+    @user = User.find(params[:user_id])
+    @next_post = @user.posts.where('id > ?', @post.id).first
+    @prev_post = @user.posts.where('id < ?', @post.id).last
   end
 end
