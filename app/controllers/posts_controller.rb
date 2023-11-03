ea3dc5b02cf63ba1959fedc9ef1c03d072e0ea88
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-03 13:14:00 +0430

    Refactor PostsController to use before_action for finding user and add new and create actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,37 @@
-# app/controllers/posts_controller.rb
-
 class PostsController < ApplicationController
-  def index
-    @user = User.find(params[:user_id])
+  before_action :find_user
 
+  def index
     @posts = @user.posts.page(params[:page]).per(3)
   end
 
-  def show
-    @post = Post.find(params[:id])
+  def create
+    @post = @user.posts.build(post_params)
+    if @post.save
+      redirect_to user_post_path(@user, @post), notice: 'Post was successfully created.'
+    else
+      flash.now[:alert] = 'Post creation failed!'
+      render :new
+    end
+  end
 
-    @user = User.find(params[:user_id])
+  def show
+    @post = @user.posts.find(params[:id])
     @next_post = @user.posts.where('id > ?', @post.id).first
     @prev_post = @user.posts.where('id < ?', @post.id).last
+  end
+
+  def new
+    @post = @user.posts.build
+  end
+
+  private
+
+  def find_user
+    @user = User.find(params[:user_id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :content)
   end
 end
