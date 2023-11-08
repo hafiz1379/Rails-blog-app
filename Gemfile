d988bf75ca950c9711e2064d7b29bac33614caa3
hafiz1379 <hafizrasa1379@gmail.com>
2023-11-08 19:57:49 +0430

    Remove empty line and comment unnecessary code in Gemfile and User model.

--- Gemfile
+++ Gemfile
@@ -58,7 +58,6 @@ group :development, :test do
   gem 'rails-controller-testing'
   gem 'rspec-rails', '~> 5.0'
   gem 'shoulda-matchers', '~> 4.0'
-
 end
 
 group :development do
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    # Include default devise modules. Others available are:
+  # Include default devise modules. Others available are:
   # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
